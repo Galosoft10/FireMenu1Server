0b0efc6626aa026df8de2e91c24c5c9ba3182880
josetrinidad <j_t_g_l@yahoo.com.mx>
2018-04-13 19:55:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jose/firemenu1server/Common/Common.java
@@ -0,0 +1,17 @@
+package com.example.jose.firemenu1server.Common;
+
+import com.example.jose.firemenu1server.Model.User;
+
+/**
+ * Created by josetrinidad on 12/03/18.
+ */
+
+public class Common {
+
+    public static User currentUser;
+
+    public static final String UPDATE = "update";
+    public static final String DELETE = "delete";
+    public static final int PICK_IMAGE_REQUEST = 71;
+
+}
---
+++ app/src/main/java/com/example/jose/firemenu1server/Interface/ItemClickListener.java
@@ -0,0 +1,12 @@
+package com.example.jose.firemenu1server.Interface;
+
+import android.view.View;
+
+/**
+ * Created by josetrinidad on 19/03/18.
+ */
+
+public interface ItemClickListener {
+
+    void onClick(View view, int position, boolean isLongClick);
+}
